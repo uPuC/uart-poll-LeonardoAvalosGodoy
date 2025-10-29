a0b35c867a4c60c095feec2ecb232b4740bb4dc9
Leonardo Avalos Godoy <99428322+LeonardoAvalosGodoy@users.noreply.github.com>
2025-10-29 13:23:08 -0700

    UART.h

--- UART.h
+++ UART.h
@@ -1,25 +1,27 @@
-// Prototypes
-// Initialization
-UART_Ini(uint8_t com, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop)
+#ifndef UART_H
+#define UART_H
 
-// Send
-void UART_puts(uint8_t com, char *str);
+#include <stdint.h>
+
+
+void UART_Ini(uint8_t com, uint32_t baudrate, uint8_t size, uint8_t parity, uint8_t stop);
+
+
 void UART_putchar(uint8_t com, char data);
+void UART_puts(uint8_t com, char *str);
 
-// Received
+
 uint8_t UART_available(uint8_t com);
-char UART_getchar(uint8_t com );
-void UART_gets(uint8_t com, char *str);
+char    UART_getchar(uint8_t com);
+void    UART_gets(uint8_t com, char *str);
 
-// Escape sequences
-UART_clrscr( uint8_t com );
-UART_setColor(uint8_t com, uint8_t color);
-UART_gotoxy(uint8_t com, uint8_t x, uint8_t y);
 
-#define YELLOW  0 // Fixme 
-#define GREEN   0 // Fixme 
-#define BLUE    0 // Fixme 
+void UART_clrscr(uint8_t com);
+void UART_setColor(uint8_t com, uint8_t color);  
+void UART_gotoxy(uint8_t com, uint8_t x, uint8_t y);
 
-// Utils
-void itoa(uint16_t number, char* str, uint8_t base) 
-uint16_t atoi(char *str)
+
+void     itoa(uint16_t number, char* str, uint8_t base);  
+uint16_t atoi(char *str);                               
+
+#endif 
